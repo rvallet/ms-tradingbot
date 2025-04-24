ac71f392bec80abda1ee42c5e2109ba9161419db
Rémy VALLET <remy.vallet@gmail.com>
2025-04-24 16:08:26 +0200

    Initial commit

---
+++ src/main/java/com/fts/ms_tradingbot/MsTradingbotApplication.java
@@ -0,0 +1,13 @@
+package com.fts.ms_tradingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTradingbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTradingbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fts/ms_tradingbot/MsTradingbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fts.ms_tradingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTradingbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
