445184b7b548aa048f2419cfb55d57d3774a5feb
Rémy VALLET <remy.vallet@gmail.com>
2025-04-25 12:00:29 +0200

    Ajustement après alimentation de la collection CryptoSymbols en BDD

--- src/main/java/com/fts/ms_tradingbot/pojo/CryptoSymbol.java
+++ src/main/java/com/fts/ms_tradingbot/pojo/CryptoSymbol.java
@@ -3,12 +3,94 @@ package com.fts.ms_tradingbot.pojo;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "crypto_symbols")
+import java.util.Date;
+
+@Document(collection = "CryptoSymbols")
 public class CryptoSymbol {
 
     @Id
     private String id;
     private String symbol;
     private String name;
+    private boolean is_active;
+    private Double start_price;
+    private Double current_price;
+    private Double percentage_change;
+    private Date created_at;
+    private Date updated_at;
 
+    public CryptoSymbol() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public Double getStart_price() {
+        return start_price;
+    }
+
+    public void setStart_price(Double start_price) {
+        this.start_price = start_price;
+    }
+
+    public Double getCurrent_price() {
+        return current_price;
+    }
+
+    public void setCurrent_price(Double current_price) {
+        this.current_price = current_price;
+    }
+
+    public Double getPercentage_change() {
+        return percentage_change;
+    }
+
+    public void setPercentage_change(Double percentage_change) {
+        this.percentage_change = percentage_change;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
 }
--- src/main/java/com/fts/ms_tradingbot/ws/CryptoSymbolApi.java
+++ src/main/java/com/fts/ms_tradingbot/ws/CryptoSymbolApi.java
@@ -2,13 +2,7 @@ package com.fts.ms_tradingbot.ws;
 import com.fts.ms_tradingbot.pojo.CryptoSymbol;
 import com.fts.ms_tradingbot.service.impl.CryptoSymbolServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,9 +16,8 @@ public class CryptoSymbolApi {
     private CryptoSymbolServiceImpl cryptoSymbolService;
 
     @GetMapping
-    public ResponseEntity<List<CryptoSymbol>> getAllCryptoSymbols() {
-        List<CryptoSymbol> symbols = cryptoSymbolService.getAllCryptoSymbols();
-        return ResponseEntity.ok(symbols);
+    public List<CryptoSymbol> getAllCryptoSymbols() {
+        return cryptoSymbolService.getAllCryptoSymbols();
     }
 
 }
