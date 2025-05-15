0878eed386b1f5883d70de69511483837539fe8f
Rémy VALLET <remy.vallet@gmail.com>
2025-05-15 10:42:50 +0200

    Exemple de critères custom Mongo : getCryptoSymbolsWithCustomConditions
    Exemple de critères custom Mongo : getCryptoSymbolsWithCustomConditions

--- src/main/java/com/fts/ms_tradingbot/dao/CryptoSymbolRepository.java
+++ src/main/java/com/fts/ms_tradingbot/dao/CryptoSymbolRepository.java
@@ -2,19 +2,18 @@ package com.fts.ms_tradingbot.dao;
 
 import com.fts.ms_tradingbot.pojo.CryptoSymbol;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CryptoSymbolRepository extends MongoRepository<CryptoSymbol, String> {
 
+    /**
+     * Récupère un CryptoSymbol par son symbole.
+     * @param symbol - le symbole de la crypto-monnaie
+     * @return - CryptoSymbol
+     */
     Optional<CryptoSymbol> findBySymbolIgnoreCase(String symbol);
-
-    @Query(value = "{}",
-            fields = "{ 'symbol' : 1 }")
-    List<CryptoSymbol> findUniqueSymbols();
 
 }
